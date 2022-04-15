823db798cf5889ee0826e2f6dca7331e88f77f12
ahmedovv123 <ahmedovv123@gmail.com>
2022-04-15 10:18:25 +0300

    feat: Upload whole project

---
+++ src/http/response.rs
@@ -0,0 +1,24 @@
+use super::StatusCode;
+use std::io::{Write, Result as IoResult };
+use std::{fmt::{Display, Formatter, Result as FmtResult }, net::TcpStream};
+
+#[derive(Debug)]
+pub struct Response {
+    status_code: StatusCode,
+    body: Option<String>,
+}
+
+impl Response {
+    pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
+        Self { status_code, body }
+    }
+
+    pub fn send(&self, stream: &mut impl Write) -> IoResult<()> {
+        let body = match &self.body {
+            Some(b) => b,
+            None => "",
+        };
+
+        write!(stream, "HTTP/1.1 {} {}\r\n\r\n{}", self.status_code, self.status_code.reason_phrase(), body)
+    }
+}
